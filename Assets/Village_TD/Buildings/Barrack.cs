ab545b849abfef6db90a7eb2df93dc35a3ca5580
FT-Audios <ft-audios@telenet.be>
2016-03-17 11:44:14 +0100

    17/03/2016
    By FT

--- Assets/Village_TD/Buildings/Barrack.cs
+++ Assets/Village_TD/Buildings/Barrack.cs
@@ -9,7 +9,13 @@ namespace Village_TD
 {
     class Barrack:Building
     {
-        private int swordfighters; //temporary
+        private int numSwordfighters = 6;   //temporary value
+        private int numArchers = 4;         //temporary value
+        private int numKnights = 3;         //temporary value
+        private int[] swordfighterCost = new int[3];
+        private int[] archerCost = new int[3];
+        private int[] knightCost = new int[3];
+
         public string numberToCreateSwordfighters;
         public string numberToCreateArchers;
         public string numberToCreateKnights;
@@ -18,36 +24,53 @@ namespace Village_TD
             return 3; // The number 3 is dependable on the number of unique type of troops, 1 type is unlocked each level
         }
 
-        public int Swordfighters
+        public int NumSwordfighters
         {
-            get
-            {
-                return swordfighters;
-            }
-
-            set
-            {
-                swordfighters = value;
-            }
+            get{ return numSwordfighters; }
+            set{ numSwordfighters = value; }
         }
-
+        public int NumArchers
+        {
+            get { return numArchers; }
+            set { numArchers = value; }
+        }
+        public int NumKnights
+        {
+            get { return numKnights; }
+            set { numKnights = value; }
+        }
+        public int[] SwordfighterCost
+        {
+            get { return swordfighterCost; }
+            set { swordfighterCost = value; }
+        }
+        public int[] ArcherCost
+        {
+            get { return archerCost; }
+            set { archerCost = value; }
+        }
+        public int[] KnightCost
+        {
+            get { return knightCost; }
+            set { knightCost = value; }
+        }
 
         public void createSwordfighter()
         {
-
-            GameObject.Find("Swordfighters").GetComponent<Swordfighter>().amSwordfighters += Convert.ToInt32(numberToCreateSwordfighters);
-            Debug.Log(GameObject.Find("Swordfighters").GetComponent<Swordfighter>().amSwordfighters);
+            /*
+            meObject.Find("Swordfighters").GetComponent<Swordfighter>().amSwordfighters += Convert.ToInt32(numberToCreateSwordfighters);
+            Debug.Log(GameObject.Find("Swordfighters").GetComponent<Swordfighter>().amSwordfighters);*/
         }
         
         public void createArcher()
         {
-            GameObject.Find("Archers").GetComponent<Archer>().amArchers += Convert.ToInt32(numberToCreateArchers);
+            //ameObject.Find("Archers").GetComponent<Archer>().amArchers += Convert.ToInt32(numberToCreateArchers);
 
         }
 
         public void createKnight()
         {
-            GameObject.Find("Knights").GetComponent<Knight>().amKnights += Convert.ToInt32(numberToCreateKnights);
+            //meObject.Find("Knights").GetComponent<Knight>().amKnights += Convert.ToInt32(numberToCreateKnights);
 
         }
     }
--- Assets/Village_TD/Buildings/House.cs
+++ Assets/Village_TD/Buildings/House.cs
@@ -9,8 +9,20 @@ namespace Village_TD
 {
     class House:Building
     {
-        
+        private int numWorkers;
+        private int numPopulation;
         public static readonly int[] maxLevelPopulations = { 25, 50, 100, 200, 350, 500, 700, 950, 1200, 1500 };//array om te bepalen bij welk level, welke maximum populatie telt
+
+        public int NumWorkers
+        {
+            get { return numWorkers; }
+            set { numWorkers = value; }
+        }
+        public int NumPopulation
+        {
+            get { return numPopulation; }
+            set { numPopulation = value; }
+        }
 
         public override int maxLevel()  //maxlevel is defined with the length of the array maxLevelPopulations
         {
@@ -25,6 +37,29 @@ namespace Village_TD
             }   
         }
 
-        
+        void setNumWorkers()
+        {
+            /*
+            //aantal werknemers van elk gebouw ophalen, ergens in het project
+            AmWorkers = GameObject.Find("workers").GetComponent<Barrack>().initialWorkersPerLevel   //aantal werknemers per level van een kazerne
+            + GameObject.Find("workers").GetComponent<House>().initialWorkersPerLevel               //aantal werknemers per level van een huis
+            + GameObject.Find("workers").GetComponent<Warehouse>().initialWorkersPerLevel           //aantal werknemers per level van de opslagplaats
+            + GameObject.Find("workers").GetComponent<clayPit>().initialWorkersPerLevel             //aantal werknemers per level van een clay
+            + GameObject.Find("workers").GetComponent<ironMine>().initialWorkersPerLevel            //aantal werknemers per level van een iron
+            + GameObject.Find("workers").GetComponent<LumberMill>().initialWorkersPerLevel;         //aantal werknemers per level van een wood
+            */
+        }
+
+        void setNumPopulation()
+        {
+            /*
+            amPopulation = GameObject.Find("amTroops").GetComponent<Combatant>().AmTroops;
+            //    + GameObject.Find("amWorkers").GetComponent<Worker>().AmWorkers;
+            //amPopulation = 10;      //-------------TEST--------------//
+            population.text = "Total amount population: " + amPopulation.ToString();
+            */
+        }
+
+
     }
 }
