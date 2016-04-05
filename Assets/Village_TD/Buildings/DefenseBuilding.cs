b873269dbdd987f70cb8332b13b52439d27c34e4
FT-Audios <ft-audios@telenet.be>
2016-04-05 12:04:53 +0200

    5042016

---
+++ Assets/Village_TD/Buildings/DefenseBuilding.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Village_TD
+{
+    class DefenseBuilding:Building
+    {
+        int defenseFactor;
+
+        public int DefenseFactor
+        {
+            get{return defenseFactor;}
+            set{defenseFactor = value;}
+        }
+    }
+}
